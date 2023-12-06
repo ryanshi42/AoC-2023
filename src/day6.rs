419801381caa099f273a0e38f51a19ca404fa96f
Ryan Shi <ryan.shi42@gmail.com>
2023-12-06 11:06:33 +1100

    test building set up, remove unnecessary files

--- src/day6.rs
+++ src/day6.rs
@@ -1,12 +1,21 @@
 use std::collections::HashMap;
 
-#[aoc(day1, part1)]
+#[aoc(day6, part1)]
 fn part1(_input: &str) -> i32 {
     return 0;
 }
 
-#[aoc(day1, part2)]
+#[aoc(day6, part2)]
 fn part2(input: &str) -> i32 {
     let mut x = 0;
     return x;
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let c = 1;
+        assert_eq!(c, 1);
+    }
 }
