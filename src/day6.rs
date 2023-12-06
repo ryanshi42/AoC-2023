5f4d5798d9ef5d09987c062122ef462dd18f1ffd
Ryan Shi <ryan.shi42@gmail.com>
2023-12-06 23:35:44 +1100

    day 6 done, join and split whitespace

--- src/day6.rs
+++ src/day6.rs
@@ -1,13 +1,46 @@
-use std::collections::HashMap;
 
 #[aoc(day6, part1)]
-fn part1(_input: &str) -> i32 {
-    return 0;
+fn part1(input: &str) -> i32 {
+    let (times, distances) = input.split_once("\n").unwrap();
+    let times = times.split_whitespace().skip(1);
+    let mut distances = distances.split_whitespace().skip(1);
+    // println!("{times:?} {distances:?}");
+
+    let mut res = 1;
+
+    for t in times {
+        let d = distances.next().unwrap();
+        let mut x = 0;
+        for i in 1..=t.parse().unwrap() {
+            if (t.parse::<i32>().unwrap() - i) * i >= d.parse().unwrap() {
+                x += 1;
+            } 
+        }
+        res *= x;
+
+    }
+
+    return res;
+
 }
 
 #[aoc(day6, part2)]
 fn part2(input: &str) -> i32 {
+    let (times, distances) = input.split_once("\n").unwrap();
+    let times = times.split_whitespace().skip(1);
+    let distances = distances.split_whitespace().skip(1);
+    let times = times.collect::<Vec<_>>().join("");
+    let distances = distances.collect::<Vec<_>>().join("");
+    // println!("{times:?} {distances:?}");
+
+    let d:i64 = distances.parse().unwrap();
     let mut x = 0;
+    for i in 1..=times.parse().unwrap() {
+        if (times.parse::<i64>().unwrap() - i) * i >= d {
+            x += 1;
+        } 
+    }
+
     return x;
 }
 
